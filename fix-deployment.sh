31e42099541e3cd6f2e7298b32bf46baac2a6c8a
shibil-basith <shibilbasith4u@gmail.com>
2026-03-08 06:05:56 +0000

    fix: resolve YAML unmarshal error in deployment templates

--- fix-deployment.sh
+++ fix-deployment.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+for dir in services/sock-shop/*; do
+  if [ -d "$dir/templates" ]; then
+    cat << 'INNER_EOF' > "$dir/templates/deployment.yaml"
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  name: {{ include "cart-db.fullname" . | default (basename $dir) }}
+  name: {{ include "$(basename $dir).fullname" . | default (basename $dir) }}
   labels:
-    {{- include "cart-db.labels" . | nindent 4 | default "app: sock-shop" }}
+    {{- include "$(basename $dir).labels" . | nindent 4 | default "app: sock-shop" }}
 spec:
   replicas: {{ .Values.replicaCount | default 1 }}
   selector:
@@ -19,3 +23,8 @@ spec:
           image: "{{ .Values.image.repository }}:{{ .Values.image.tag }}"
           ports:
             - containerPort: {{ .Values.service.targetPort }}
+INNER_EOF
+    # Correct the dynamic naming inside the heredoc
+    sed -i "s/\$(basename \$dir)/$(basename $dir)/g" "$dir/templates/deployment.yaml"
+  fi
+done
